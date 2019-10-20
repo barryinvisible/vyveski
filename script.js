9b498d880f888409caa792dbc26b97e1d9c1b441
barryinvisible <arturdavid96@gmail.com>
2019-10-20 18:12:46 +0300

    add

--- script.js
+++ script.js
@@ -4,10 +4,11 @@ jQuery(function() {
         slidesToShow: 4,
         slidesToScroll: 2,
         touchMove: true,
+        prevArrow: '<button class="slick-prev slick-arrow" aria-label="Previous" type="button" style="flex: none;"><i class="fas fa-arrow-right"></i></button>',
+        nextArrow: '<button class="slick-prev slick-arrow" aria-label="Next" type="button" style="flex: none;"><i class="fas fa-arrow-left"></i></button>',
         responsive: [{
                 breakpoint: 960,
                 settings: {
-                    infinite: true,
                     slidesToShow: 3,
                     slidesToScroll: 2
                 }
@@ -15,7 +16,6 @@ jQuery(function() {
             {
                 breakpoint: 720,
                 settings: {
-                    infinite: true,
                     slidesToShow: 2,
                     slidesToScroll: 1
                 }
@@ -23,14 +23,13 @@ jQuery(function() {
         ]
     });
 
-    $('.slick-prev, .slick-next').html('<i class="fas fa-arrow-right"></i>');
     $('slick-prev').appendTo('.slick-list');
-    $('.icon').on('click', function() {
+    $('.burger-icon').on('click', function() {
         $(this).toggleClass('toggleIcon');
-        $('.header-nav').toggleClass('burger-nav');
+        // $('.header-nav').toggleClass('burger-nav');
     });
-    let windowWidth = $(window).width();
-    if (windowWidth <= 960) {
-        $('.header-location, .header-contact').appendTo('.header-nav');
-    }
+    // let windowWidth = $(window).width();
+    // if (windowWidth <= 960) {
+    //     $('.header-location, .header-contact').appendTo('.header-nav');
+    // }
 });
